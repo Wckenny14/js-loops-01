f5b3c706d56c8558d283a668a8e2ec5101beb118
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2022-12-07 16:27:10 +1100

    class work

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Every 2
+// for (let i = 0; i < 10; i+=2) {
+//     console.log(i)
+// }
+
+
+// 9 to 3
+// for (let i = 9; i > 3; i--) {
+//     console.log(i)
+// }
+
+// 9 times table
+// for (let i = 0; i < 10; i++) {
+//     let nineMultiply = i * 9;
+//     console.log(`9 times ${i} is ${nineMultiply} `)
+// }
+
+
+// Even&Odd Reporter
+// for (let i = 1; i < 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i} is even`)
+//     } else {
+//         console.log(`${i} is odd`)
+//     }
+// }
+
+
